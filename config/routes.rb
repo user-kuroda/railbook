e165f4958ab818d1e29c8ef080dc552972c07e87
Kuroda Moeko <kuroda-moeko@outlook.jp>
2016-06-12 14:18:16 +0900

    rensh1 2ndCommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :books do
     get 'search', on: :collection
-  #end
-  #resources :books do
     get 'view_helper_test', on: :collection
   end
   # The priority is based upon order of creation: first created -> highest priority.
