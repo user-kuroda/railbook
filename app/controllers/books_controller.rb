0463e5159f604347dc3fd222f1ff35b0ac032601
Kuroda Moeko <kuroda-moeko@outlook.jp>
2016-07-06 10:20:11 +0900

    DBあれこれ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -62,7 +62,7 @@ class BooksController < ApplicationController
   end
 
   def search
-   @books = Book.where(title: params["search"]["title"])
+   @books = Book.where("title like '%#{params["search"]["title"]}%'")
    @search = params["search"]["title"]
    render :index
   end
